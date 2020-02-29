5bc843f25cec7c3bd77dfa84c21aabb144519ffe
Luis Uguina <luis.git@uguina.com>
2020-02-29 21:08:00 +1100

    feat: add the 'global stats' section

--- pySheepIt.py
+++ pySheepIt.py
@@ -13,6 +13,8 @@ class RenderWidget(QWidget):
         # stack the different panels vertically
         main_layout = QVBoxLayout()
         main_layout.addWidget(self.create_project_section())
+        main_layout.addSpacing(15)
+        main_layout.addWidget(self.create_global_stats_section())
 
         self.setLayout(main_layout)
 
@@ -52,6 +54,35 @@ class RenderWidget(QWidget):
         gbox_active_project.setLayout(layout)
 
         return gbox_active_project
+
+    def create_global_stats_section(self):
+        gbox_blobal_stats = QGroupBox('Global Stats')
+
+        lbl_active_projects = QLabel('Active projects:')
+        self.lbl_active_projects_value = QLabel('')
+
+        lbl_connected_machines = QLabel('Connected machines:')
+        self.lbl_connected_machines_value = QLabel('')
+
+        lbl_remaining_frames = QLabel('Remaining frames:')
+        self.lbl_remaining_frames_value = QLabel('')
+
+        lbl_user_points = QLabel('User\'s total points:')
+        self.lbl_user_points_value = QLabel('')
+
+        layout = QGridLayout()
+        layout.addWidget(lbl_active_projects, 0, 0, Qt.AlignRight)
+        layout.addWidget(self.lbl_active_projects_value, 0, 1, 1, 3, Qt.AlignLeft)
+        layout.addWidget(lbl_connected_machines, 1, 0, Qt.AlignRight)
+        layout.addWidget(self.lbl_connected_machines_value, 1, 1, 1, 3, Qt.AlignLeft)
+        layout.addWidget(lbl_remaining_frames, 2, 0, Qt.AlignRight)
+        layout.addWidget(self.lbl_remaining_frames_value, 2, 1, 1, 3, Qt.AlignLeft)
+        layout.addWidget(lbl_user_points, 3, 0, Qt.AlignRight)
+        layout.addWidget(self.lbl_user_points_value, 3, 1, 1, 3, Qt.AlignLeft)
+
+        gbox_blobal_stats.setLayout(layout)
+
+        return gbox_blobal_stats
 
 
 class PySheepIt(QMainWindow):
